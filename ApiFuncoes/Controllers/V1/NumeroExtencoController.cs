1f672096131ffe0611910c1fefadf0ae3ed6b397
jucimario <jucimarioo@live.com>
2023-06-03 13:09:06 -0300

    EstruturaDoProjeto

---
+++ ApiFuncoes/Controllers/V1/NumeroExtencoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFuncoes.Controllers.V1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class NumeroExtencoController : ControllerBase
+    {
+
+        [HttpGet("{NumeroExtenso}")]
+        public ActionResult<string> Get(int numero)
+        {
+            return NoContent();
+        }
+    }
+}

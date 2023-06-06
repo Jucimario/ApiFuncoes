67d79c30f276d964798363cab56495d53c7dfcc5
jucimario <jucimarioo@live.com>
2023-06-05 23:46:49 -0300

    IncluindoTesteUnitarioNumeroPOrExtensoESomaArray

---
+++ Units/TestMock.cs
@@ -0,0 +1,15 @@
+﻿using Moq.AutoMock;
+
+namespace Units;
+
+public class TestMock : IDisposable
+{
+    private readonly AutoMocker _mocker = new AutoMocker();
+
+    public T ObterInstanciaMock<T>() where T : class => _mocker.CreateInstance<T>();
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
